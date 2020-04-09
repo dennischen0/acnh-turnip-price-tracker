0d201b62653834e396727268ed88fa49fcb4e75d
Dennis Chen <c.w.dennis@gmail.com>
2020-04-08 21:29:45 -0700

    initial setup

---
+++ build/precache-manifest.8d4c6ef3cefab9608d0f6cf29e532572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91369dc70263a55ec5ead2bf984717b4",
+    "url": "/dennischen0/acnh-turnip-price-trackerindex.html"
+  },
+  {
+    "revision": "b205d4270f2b3808a670",
+    "url": "/dennischen0/acnh-turnip-price-trackerstatic/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "94e6e1973f80a513b8cc",
+    "url": "/dennischen0/acnh-turnip-price-trackerstatic/js/2.a14e3aed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dennischen0/acnh-turnip-price-trackerstatic/js/2.a14e3aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b205d4270f2b3808a670",
+    "url": "/dennischen0/acnh-turnip-price-trackerstatic/js/main.0b7cad47.chunk.js"
+  },
+  {
+    "revision": "4081c2090b727e07486a",
+    "url": "/dennischen0/acnh-turnip-price-trackerstatic/js/runtime-main.bdd14f3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dennischen0/acnh-turnip-price-trackerstatic/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/test.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class Test extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            result: ''
+        }
+    }
+
+    componentDidMount() {
+        this.ping()
+    }
+
+    ping() {
+        fetch("/ping")
+        .then(res => res.text())
+        .then((res => {
+            this.setState({
+                isLoaded: true,
+                result: res
+            })
+        }))
+    }
+
+    render() {
+        const { isLoaded, result } = this.state;
+
+        if(!isLoaded) {
+            return <div>Waiting for pong</div>
+        } else {
+            return <div>{result}</div>
+        }
+    }
+}
+
+export default Test
