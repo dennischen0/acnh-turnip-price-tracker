2cad46701c0596bd904d7eb7c790838d806c4c51
Dennis Chen <c.w.dennis@gmail.com>
2020-04-09 01:17:37 -0700

    Redirect to https

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 
+app.use(function(req, res, next) {
+  if ((req.get('X-Forwarded-Proto') !== 'https')) {
+    res.redirect('https://' + req.get('Host') + req.url);
+  } else
+    next();
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
