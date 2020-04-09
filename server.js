27591a9adcf7cf0f2535d14f88c10b47280f07f7
Dennis Chen <c.w.dennis@gmail.com>
2020-04-09 01:41:30 -0700

    Server

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 
+app.use();
+
 app.use (function (req, res, next) {
-  express.static(path.join(__dirname, 'build'))
   if (req.secure) {
     // request was via https, so do no special handling
+    express.static(path.join(__dirname, 'build'));
     next();
   } else {
     // request was via http, so redirect to https
@@ -17,6 +19,8 @@ app.use (function (req, res, next) {
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
