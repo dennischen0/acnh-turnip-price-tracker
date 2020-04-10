97297aae4bf95cc45458fc681b39e033585c3448
Dennis Chen <c.w.dennis@gmail.com>
2020-04-10 00:12:37 -0700

    Remove https redirect

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 
-app.use(function(req, res, next) {
-  if ((req.get('X-Forwarded-Proto') !== 'https')) {
-    res.redirect('https://' + req.get('Host') + req.url);
-  } else
+// app.use(function(req, res, next) {
+//   if ((req.get('X-Forwarded-Proto') !== 'https')) {
+//     res.redirect('https://' + req.get('Host') + req.url);
+//   } else
   
-    next();
-});
+//     next();
+// });
 
 app.use(express.static(path.join(__dirname, 'build')));
 
