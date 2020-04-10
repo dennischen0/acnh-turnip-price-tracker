9582a6232b12c709fc8977d66a5737843425385f
Dennis Chen <c.w.dennis@gmail.com>
2020-04-10 00:27:51 -0700

    https only on prod

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+require('dotenv').config();
 
-// app.use(function(req, res, next) {
-//   if ((req.get('X-Forwarded-Proto') !== 'https')) {
-//     res.redirect('https://' + req.get('Host') + req.url);
-//   } else
-  
-//     next();
-// });
+app.use(function(req, res, next) {
+  if (process.env.NODE_ENV === 'prod' && (req.get('X-Forwarded-Proto') !== 'https')) {
+    res.redirect('https://' + req.get('Host') + req.url);
+  } else{
+    next();
+  }
+});
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -17,7 +18,8 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 
-app.get('/', function (req, res) {
+// Leave this at the end of the file
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
