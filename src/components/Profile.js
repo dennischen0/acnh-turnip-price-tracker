b017dae6c2f4c5dcb2a13a02a0ae410637c6f980
Dennis Chen <c.w.dennis@gmail.com>
2020-04-10 00:58:15 -0700

    Add loading screen to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,14 @@
 // src/components/Profile.js
 
 import React, { Fragment } from "react";
+import Loading from "../components/Loading";
 import { useAuth0 } from "../react-auth0-spa";
 
 const Profile = () => {
   const { loading, user } = useAuth0();
 
   if (loading || !user) {
-    return <div>Loading...</div>;
+    return <Loading />;
   }
 
   return (
