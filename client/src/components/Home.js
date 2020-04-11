d46256890a01714676fa2350d70c1c453579276a
Dennis Chen <c.w.dennis@gmail.com>
2020-04-11 01:39:11 -0700

    add entry

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,19 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Week from "../components/Week";
 import Chart from "../components/Chart";
+import WeeklyEntry from "../components/WeeklyEntry";
 
 const Home = () => {
+  const [prices, setPrices] = useState(new Array(13));
 
-  var prices = new Array(13)
-  prices[0] =97
-  prices[1] =85
-  prices[2] =81
-  prices[3] =76
-  prices[4] =72
-  prices[5] =133
-  prices[6] =188
+  const updatePrices = (newPrices) => {
+    console.log("GOT NEW PRICES");
+    // prices = newPrices.slice(1);
+    console.log(prices)
+    setPrices(newPrices)
+  };
+
   return (
     <>
+      <WeeklyEntry onChange={updatePrices} />
       <Chart filter={prices} />
     </>
   );
