dc384cca9afe2dbd8c1dcc911b1a1e4f26847c63
Dennis Chen <c.w.dennis@gmail.com>
2020-04-17 16:42:35 -0700

    Fix constants for port

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,5 @@
+const apiServer = process.env.NODE_ENV === 'production' ? `https://acnh-turnip-price-tracker.herokuapp.com` : `http://localhost:8080`;
+
 module.exports = Object.freeze({
-  API_SERVER: `https://localhost`
+  API_SERVER: apiServer
 });
