a66b6402089ee42e769de28392abe914422bbf02
Dennis Chen <dennischen0@users.noreply.github.com>
2020-04-17 20:59:02 -0700

    Revert "Fix constants for port"
    This reverts commit dc384cca9afe2dbd8c1dcc911b1a1e4f26847c63.

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,3 @@
-const apiServer = process.env.NODE_ENV === 'production' ? `https://acnh-turnip-price-tracker.herokuapp.com` : `http://localhost:8080`;
-
 module.exports = Object.freeze({
-  API_SERVER: apiServer
+  API_SERVER: `https://localhost`
 });
