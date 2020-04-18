ddb606004df1b85619c3ada9ba5881c8fa150de9
Dennis Chen <c.w.dennis@gmail.com>
2020-04-17 18:03:29 -0700

    Final fix?

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,3 @@
-const apiServer = process.env.NODE_ENV === 'production' ? `https://acnh-turnip-price-tracker.herokuapp.com` : `http://localhost:8080`;
-
 module.exports = Object.freeze({
-  API_SERVER: apiServer
+  API_SERVER: process.env.REACT_APP_API_SERVER
 });
