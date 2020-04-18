979458ca0855261faa6f735da9d868d9781a72b1
Dennis Chen <dennischen0@users.noreply.github.com>
2020-04-17 20:59:02 -0700

    Revert "Final fix?"
    This reverts commit ddb606004df1b85619c3ada9ba5881c8fa150de9.

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,5 @@
+const apiServer = process.env.NODE_ENV === 'production' ? `https://acnh-turnip-price-tracker.herokuapp.com` : `http://localhost:8080`;
+
 module.exports = Object.freeze({
-  API_SERVER: process.env.REACT_APP_API_SERVER
+  API_SERVER: apiServer
 });
